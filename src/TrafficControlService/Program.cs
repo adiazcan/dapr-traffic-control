09379c12eecba45c67b6bce54d92e5ea608af498
Alberto Diaz Martin <alberto.diaz@encamina.com>
2022-07-18 18:02:21 +0200

    changes on traffic service

--- src/TrafficControlService/Program.cs
+++ src/TrafficControlService/Program.cs
@@ -1,36 +1,57 @@
-// create web-app
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<ISpeedingViolationCalculator>(
-    new DefaultSpeedingViolationCalculator("A12", 10, 100, 5));
-
-builder.Services.AddSingleton<IVehicleStateRepository, DaprVehicleStateRepository>();
-
-var daprHttpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3600";
-var daprGrpcPort = Environment.GetEnvironmentVariable("DAPR_GRPC_PORT") ?? "60000";
-builder.Services.AddDaprClient(builder => builder
-    .UseHttpEndpoint($"http://localhost:{daprHttpPort}")
-    .UseGrpcEndpoint($"http://localhost:{daprGrpcPort}"));
-
-builder.Services.AddControllers();
-
-builder.Services.AddActors(options =>
+try
 {
-    options.Actors.RegisterActor<VehicleActor>();
-});
+    Console.WriteLine("INIT Traffic Progam.cs");
+    // create web-app
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Services.AddSingleton<ISpeedingViolationCalculator>(
+        new DefaultSpeedingViolationCalculator("A12", 10, 100, 5));
 
-// configure web-app
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
+    Console.WriteLine("Added ISpeedingViolationCalculator");
+
+    builder.Services.AddSingleton<IVehicleStateRepository, DaprVehicleStateRepository>();
+
+    Console.WriteLine("Added IVehicleStateRepository");
+
+
+    var daprHttpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3600";
+    var daprGrpcPort = Environment.GetEnvironmentVariable("DAPR_GRPC_PORT") ?? "60000";
+    builder.Services.AddDaprClient(builder => builder
+        .UseHttpEndpoint($"http://localhost:{daprHttpPort}")
+        .UseGrpcEndpoint($"http://localhost:{daprGrpcPort}"));
+
+    Console.WriteLine("Added DarpClient");
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddActors(options =>
+    {
+        options.Actors.RegisterActor<VehicleActor>();
+    });
+
+    Console.WriteLine("Added Actors");
+
+    var app = builder.Build();
+
+    // configure web-app
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    app.UseCloudEvents();
+
+    // configure routing
+    app.MapControllers();
+    app.MapActorsHandlers();
+
+    // let's go!
+    app.Run();    
+
+    Console.WriteLine("App run");
 }
-app.UseCloudEvents();
+catch (System.Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+    throw;
+}
 
-// configure routing
-app.MapControllers();
-app.MapActorsHandlers();
-
-// let's go!
-app.Run();
